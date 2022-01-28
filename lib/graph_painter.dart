ce132ad9be18d62db9c80fdadad6fc4e4e159e65
sample <sample@sample.com>
2022-01-28 11:12:51 +0900

    feat: paint circle

--- lib/graph_painter.dart
+++ lib/graph_painter.dart
@@ -5,12 +5,17 @@ import 'package:flutter/material.dart';
 class GraphPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
-    // TODO: implement paint
+    canvas.drawCircle(
+        Offset(size.width / 2, size.height / 2),
+        50,
+        Paint()
+          ..color = Colors.amber
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 10);
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    // TODO: implement shouldRepaint
     return false;
   }
 }

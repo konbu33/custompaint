a0252238b0dfea4635d21fdef9f47580e2d33297
sample <sample@sample.com>
2022-01-28 14:42:35 +0900

    feat: create graph data

--- lib/graph_painter.dart
+++ lib/graph_painter.dart
@@ -5,6 +5,18 @@ import 'package:flutter/material.dart';
 class GraphPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
+    final now = DateTime.now();
+    Map<DateTime, double> data = {
+      now.add(Duration(days: -6)): 2,
+      now.add(Duration(days: -5)): 2,
+      now.add(Duration(days: -4)): 2,
+      now.add(Duration(days: -3)): 2,
+      now.add(Duration(days: -2)): 2,
+      now.add(Duration(days: -1)): 2,
+      now: 3,
+    };
+
+    print("$data");
     canvas.drawCircle(
         Offset(size.width / 2, size.height / 2),
         50,
